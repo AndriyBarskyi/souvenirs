88d664813dd4f283a836491fb015f54b5aa79531
AndriyBarskyi <barskyiandriy@gmail.com>
2023-10-31 21:02:32 +0200

    implement main class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,15 +3,25 @@ package org.example;
 import java.math.BigDecimal;
 import java.util.Comparator;
 
+import org.example.database.ManufacturersFileDatabase;
 import org.example.database.SouvenirsFileDatabase;
 import org.example.entity.Souvenir;
+import org.example.service.ManufacturerService;
+import org.example.service.ManufacturerServiceImpl;
+import org.example.service.SouvenirService;
+import org.example.service.SouvenirServiceImpl;
 
 public class App 
 {
     public static void main( String[] args ) {
-        System.out.println("Sorting and filtering:");
-        System.out.println(SouvenirsFileDatabase.getInstance().findAll(
-            souvenir -> souvenir.getPrice().compareTo(BigDecimal.valueOf(10.0))
-                > 0, Comparator.comparing(Souvenir::getPrice), false));
+        ManufacturerService manufacturerService = new ManufacturerServiceImpl(
+            ManufacturersFileDatabase.getInstance(),
+            SouvenirsFileDatabase.getInstance());
+        SouvenirService souvenirService = new SouvenirServiceImpl(
+            SouvenirsFileDatabase.getInstance(),
+            ManufacturersFileDatabase.getInstance());
+        ConsoleInterface consoleInterface = new ConsoleInterface(
+            manufacturerService, souvenirService);
+        consoleInterface.start();
     }
 }
