6abaf2bf5d0cdfb30ea8dbfce35703d44394a330
AndriyBarskyi <barskyiandriy@gmail.com>
2023-10-31 23:26:25 +0200

    add TODOs

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,21 @@
 package org.example;
 
-import java.math.BigDecimal;
-import java.util.Comparator;
-
 import org.example.database.ManufacturersFileDatabase;
 import org.example.database.SouvenirsFileDatabase;
-import org.example.entity.Souvenir;
 import org.example.service.ManufacturerService;
 import org.example.service.ManufacturerServiceImpl;
 import org.example.service.SouvenirService;
 import org.example.service.SouvenirServiceImpl;
 
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+
+    // Still in progress...
+    // TODO: Add unit-tests
+    // TODO: Add JavaDoc
+    // TODO: Add validation
+    // TODO: Add exception handling
+    // TODO: refactor code
+    public static void main(String[] args) {
         ManufacturerService manufacturerService = new ManufacturerServiceImpl(
             ManufacturersFileDatabase.getInstance(),
             SouvenirsFileDatabase.getInstance());
